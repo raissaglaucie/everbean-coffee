1306a3de1de1eb744d38b747031f3074253d52d9
Raissa <raissaglaucie@icloud.com>
2026-06-01 15:48:55 -0700

    register all models in django admin

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import (
+    Product,
+    Review,
+    Order,
+    OrderItem,
+    ShippingAddress,
+    UserProfile,
+)
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(Review)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
+admin.site.register(UserProfile)
